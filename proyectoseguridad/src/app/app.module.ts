32f621adc7cf046a6f686fe0f752ea85c39f4de1
DennisBonilla927 <130373702+DennisBonilla927@users.noreply.github.com>
2023-12-22 11:31:56 -0500

    diseño login

--- proyectoseguridad/src/app/app.module.ts
+++ proyectoseguridad/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"proyecto-seguridad-30035","appId":"1:1017306172688:web:304bcbbc60879f2f54e995","storageBucket":"proyecto-seguridad-30035.appspot.com","apiKey":"AIzaSyBw75xSdh1_Hth9z9d4GvkvjtewVm6E5V4","authDomain":"proyecto-seguridad-30035.firebaseapp.com","messagingSenderId":"1017306172688"})),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
