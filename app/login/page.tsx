3ac6c9719fcd51349bd8840928d21fe491a67d58
Sebastián Palate <dpalate2646@uta.edu.ec>
2023-12-31 11:19:27 -0500

    Control de Roles
    Roles asignados 1, 2 y 3.
    Problemas en la redirección al momento de mostrar la información renderizada hacia administradores y facultades.  Posibles inconvenienves para los siguientes items.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,6 @@ import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
 import 'firebase/auth';
 import signInWithMicrosoft from "../../firebase/Auth/singin";
-import { signIn } from "next-auth/react"; 
 
 import { useRouter } from 'next/navigation'
 
@@ -19,9 +18,9 @@ export default function LoginPage() {
   const [errorMensaje, setErrorMensaje] = useState('');
 
 
-  const handleLogin = async (event) => {
+  const handleLogin = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-  
+
     try {
       const response = await fetch('http://3.21.41.85/api/v1/usuario/login', {
         method: 'POST',
@@ -33,9 +32,18 @@ export default function LoginPage() {
           contrasena,
         }),
       });
-  
+
       if (response.ok) {
-        // Si la respuesta es exitosa, redirige a la página de dashboard
+
+        // Lógica para obtener el rol del usuario desde el backend
+        const data = await response.json();
+
+        // Almacena el rol en la sesión
+        const role = data.rol_id;
+
+        // Guarda el rol en localStorage
+        localStorage.setItem('userRole', role);
+
         router.push('/dashboard');
       } else {
         // Si hay un error en la respuesta, maneja el error (puedes mostrar un mensaje, por ejemplo)
@@ -46,7 +54,7 @@ export default function LoginPage() {
       console.error('Error al conectarse con el servicio:', error);
     }
   };
-  
+
 
   const handleForm = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
@@ -55,21 +63,21 @@ export default function LoginPage() {
     if (error) {
       console.log('error');
       return console.log(error);
-      
+
     }
     // else successful
     console.log('correcto');
     console.log(result);
-    
+
     router.push('/dashboard')
   };
 
 
-	return (
-		<div className="flex justify-center items-center h-screen" style={{ backgroundImage: "url('/bg.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div className="bg-white p-5 rounded-5 text-secondary shadow rounded-lg"  style={{ width: '25rem' }}>
+  return (
+    <div className="flex justify-center items-center h-screen" style={{ backgroundImage: "url('/bg.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className="bg-white p-5 rounded-5 text-secondary shadow rounded-lg" style={{ width: '25rem' }}>
         <div className="flex justify-center">
-          <Image src="/logoUta.png" alt="login-icon"  width={175} height={175} />
+          <Image src="/logoUta.png" alt="login-icon" width={175} height={175} />
         </div>
         <div className="text-center text-4xl font-bold customcolor">Login</div>
         <div className="mt-4">
@@ -78,8 +86,8 @@ export default function LoginPage() {
               <Image src="/username-icon.svg" alt="username-icon" width={16} height={16} />
             </div>
             <Input type="email" label="Email" radius="none"
-            value={correo} // Asocia el valor del estado 'correo' al campo de entrada
-            onChange={(e) => setCorreo(e.target.value)}/>
+              value={correo} // Asocia el valor del estado 'correo' al campo de entrada
+              onChange={(e) => setCorreo(e.target.value)} />
           </div>
         </div>
         <div className="mt-1">
@@ -87,9 +95,9 @@ export default function LoginPage() {
             <div className="p-2">
               <Image src="/password-icon.svg" alt="password-icon" width={16} height={16} />
             </div>
-            <Input type="password" label="Contraseña" radius="none" 
-            value={contrasena} // Asocia el valor del estado 'contrasena' al campo de entrada
-            onChange={(e) => setContrasena(e.target.value)}/>
+            <Input type="password" label="Contraseña" radius="none"
+              value={contrasena} // Asocia el valor del estado 'contrasena' al campo de entrada
+              onChange={(e) => setContrasena(e.target.value)} />
           </div>
         </div>
         <div className="flex justify-around mt-1">
@@ -100,8 +108,8 @@ export default function LoginPage() {
             {/* Otro contenido */}
           </div>
         </div>
-        <Button className="bg-custom-bg-color text-white w-full py-2 font-semibold mt-4 shadow-sm" 
-        onClick={handleLogin}>
+        <Button className="bg-custom-bg-color text-white w-full py-2 font-semibold mt-4 shadow-sm"
+          onClick={handleLogin}>
           Login
         </Button>
         <div className="flex gap-1 justify-center mt-1">
@@ -112,10 +120,10 @@ export default function LoginPage() {
             <span className="bg-white px-3 customcolor">or</span>
           </div>
         </div>
-        <Button className="bg-custom text-white w-full py-2 font-semibold mt-4 shadow-sm" 
-        onClick={handleForm}>
-        <Image src="/Outlook.svg" alt="google-icon" width={26} height={26} />
-        Continuar con Outlook
+        <Button className="bg-custom text-white w-full py-2 font-semibold mt-4 shadow-sm"
+          onClick={handleForm}>
+          <Image src="/Outlook.svg" alt="google-icon" width={26} height={26} />
+          Continuar con Outlook
         </Button>
         {errorMensaje && (
           <div className="text-red-500 text-sm text-center mt-2">{errorMensaje}</div>
@@ -123,5 +131,5 @@ export default function LoginPage() {
 
       </div>
     </div>
-	);
+  );
 }
