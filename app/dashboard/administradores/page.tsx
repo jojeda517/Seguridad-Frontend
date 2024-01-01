3162447360b8757c363c70e671623459265cd82a
DennisBonilla927 <130373702+DennisBonilla927@users.noreply.github.com>
2024-01-01 15:51:38 -0500

    commit mensajes notificaciones

--- app/dashboard/administradores/page.tsx
+++ app/dashboard/administradores/page.tsx
@@ -189,11 +189,13 @@ export default function AdministradoresPage() {
               setAdministradores((prevAdministradores) =>
               prevAdministradores.filter((administrador) => administrador.id !== id)
               );
+              mostrarMensajeToast('Administrador Eliminado');
             } else {
               throw new Error('Failed to delete');
             }
           })
           .catch((error) => console.error('Error deleting:', error));
+          mostrarMensajeToast('Error al Eliminar');
       };
     
 
@@ -212,6 +214,7 @@ export default function AdministradoresPage() {
       
           if (!response.ok) {
             throw new Error('Failed to register administrator');
+            
           }
       
           // El administrador se ha registrado exitosamente
@@ -238,6 +241,7 @@ export default function AdministradoresPage() {
           mostrarMensajeToast('Registro exitoso');
         } catch (error) {
           console.error('Error al registrar administrador:', error);
+          mostrarMensajeToast('Error al registrar');
           // Manejar el error, mostrar un mensaje de error, etc.
         }
       };
@@ -304,8 +308,10 @@ export default function AdministradoresPage() {
       
           // Cerrar el formulario después de la actualización exitosa
           setShowFormulario(false);
+          mostrarMensajeToast('Administrador Actualizado');
         } catch (error) {
           console.error('Error al actualizar administrador:', error);
+          mostrarMensajeToast('Error al actualizar');
           // Manejar el error, mostrar un mensaje de error, etc.
         }
       };
@@ -695,23 +701,45 @@ export default function AdministradoresPage() {
 
 {mostrarToast && (
   
-
-<div id="toast-default" className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-    <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200">
-        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z"/>
-        </svg>
-        <span className="sr-only">Fire icon</span>
-    </div>
-    <div className="ms-3 text-sm font-normal">Set yourself free.</div>
-    <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close"
-    onClick={() => setMostrarToast(false)}>
-        <span className="sr-only">Close</span>
-        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-        </svg>
-    </button>
+<div
+  id="toast-default"
+  className="fixed top-8 right-8 z-50 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+  role="alert"
+>
+  <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+    <svg
+      className="w-5 h-5"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+    >
+      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+    </svg>
+    <span className="sr-only">Warning icon</span>
+  </div>
+  <div className="ms-3 text-sm font-normal">{mensajeToast}.</div>
+  <button
+    type="button"
+    className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+    data-dismiss-target="#toast-default"
+    aria-label="Close"
+    onClick={() => setMostrarToast(false)}
+  >
+    <span className="sr-only">Close</span>
+    <svg
+      className="w-3 h-3"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 14 14"
+    >
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+    </svg>
+  </button>
 </div>
+
+
 
 
 )}
