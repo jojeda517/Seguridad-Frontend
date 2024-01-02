d2fc4df752453ddbc36bdbadadf7ee3b69bb2f5d
DennisBonilla927 <130373702+DennisBonilla927@users.noreply.github.com>
2024-01-02 18:56:02 -0500

    cambios icon

--- app/dashboard/estudiantes/carpetasEstudiantes/archivosEstudiante/page.tsx
+++ app/dashboard/estudiantes/carpetasEstudiantes/archivosEstudiante/page.tsx
@@ -266,7 +266,7 @@ export default function archivosPage() {
               <tr
                 key={estudiantes.id}
                 className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-                onClick={() => handleCarpetas(estudiantes)}
+                
               >
                 <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   {estudiantes.cedula}
