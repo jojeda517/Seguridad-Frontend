3efa72e86509c8aa1b320825b1c950a32cbd3614
DennisBonilla927 <130373702+DennisBonilla927@users.noreply.github.com>
2024-01-03 11:58:10 -0500

    correccion contraseña

--- app/dashboard/administradores/page.tsx
+++ app/dashboard/administradores/page.tsx
@@ -648,6 +648,20 @@ export default function AdministradoresPage() {
                 />
               </div>
               <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contrasena">
+                  Contraseña
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="contrasena"
+                  type="password"
+                  placeholder="Ingrese la contraseña"
+                  value={selectedAdministrador.contrasena}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="facultad">
                   Facultad
                 </label>
