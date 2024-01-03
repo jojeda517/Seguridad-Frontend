dabfa0b913b2063685f688f3a9b32e55baf0ecfd
DennisBonilla927 <130373702+DennisBonilla927@users.noreply.github.com>
2024-01-02 19:01:18 -0500

    cambios icono download

--- app/dashboard/administradores/DownloadIcon.jsx
+++ app/dashboard/administradores/DownloadIcon.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 
 export const DownloadIcon = (props) => (
-    <svg
-    aria-hidden="true"
-    fill="none"
-    focusable="false"
-    height="1em"
-    role="presentation"
-    viewBox="0 0 20 20"
-    width="1em"
-    {...props}
-  >
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M10 5.41L14.29 9.71L15.71 8.29L10 2.59L4.29 8.29L5.71 9.71L10 5.41ZM19 14.99H1V16.99H19V14.99Z"
-      fill="currentColor"
-    />
+
+<div className="ml-2">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path d="M10.75 2.75a.75.75 0 0 0-1.5 0v8.614L6.295 8.235a.75.75 0 1 0-1.09 1.03l4.25 4.5a.75.75 0 0 0 1.09 0l4.25-4.5a.75.75 0 0 0-1.09-1.03l-2.955 3.129V2.75Z" />
+    <path d="M3.5 12.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 4.75 18h10.5A2.75 2.75 0 0 0 18 15.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z" />
   </svg>
+</div>
+
+
 );
+
+
