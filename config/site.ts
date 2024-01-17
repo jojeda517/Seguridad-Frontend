24e6a6c4bc7726441a53d9d264f71340aad1b772
Sebastián Palate <dpalate2646@uta.edu.ec>
2024-01-16 19:19:45 -0500

    Cambio en el Nombre de la Página

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Sistema Documental UTA",
+	description: "Design experience.",
 	navItems: [
 		{
 			label: "Home",
